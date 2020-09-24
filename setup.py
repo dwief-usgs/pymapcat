3eb8d88db4b4abdbe8b91dd97fcd938b69d4540d
dwief <dwieferich@usgs.gov>
2020-09-24 08:45:33 -0600

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dwief-usgs/pymapcat',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=False,
 )
